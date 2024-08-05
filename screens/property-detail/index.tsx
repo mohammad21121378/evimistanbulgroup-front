bf47279a0bcc37154218549e6814fd25d208464d
Jose A Vazquez <ngelcodes@Joses-iMac.local>
2024-08-05 13:22:55 -0400

    DONE

--- screens/property-detail/index.tsx
+++ screens/property-detail/index.tsx
@@ -2,12 +2,14 @@ import Layout from "@/components/Layout";
 import Hero from "./hero";
 import Location from "./location";
 import Overview from "./overview";
+import AdditionalGallery from "./additional-gallery";
 
 export default function PropertyDetailPage({ item }: any) {
   return (
     <Layout>
       <Hero item={item} />
       <Overview item={item} />
+      <AdditionalGallery images={item.images} />
       <Location />
     </Layout>
   );
