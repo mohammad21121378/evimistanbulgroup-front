b04aa5c4e826cb1e9d2627326500fb1dc03db9f3
Jose A Vazquez <ngelcodes@Joses-iMac.local>
2024-08-05 08:48:13 -0400

    S

--- app/property-detail/page.tsx
+++ app/property-detail/page.tsx
@@ -1,6 +1,6 @@
 import PropertyDetailPage from "@/screens/about/property-detail";
 
-export default function PropertyDetail({ searchParams }) {
+export default function PropertyDetail({ searchParams }: any) {
   const item = JSON.parse(searchParams.item);
 
   return <PropertyDetailPage item={item} />;
