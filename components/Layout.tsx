048e98fdba8daac311dd16e6357fe126b7c40ef9
Jose A Vazquez <ngelcodes@Joses-iMac.local>
2024-08-05 20:37:55 -0400

    SW

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import React from "react";
 import Curtain from "./curtain";
 import Footer from "./footer";
 import Header from "./header";
@@ -7,9 +10,20 @@ type LayoutProps = {
 };
 
 export default function Layout({ children }: LayoutProps) {
+  const [showCurtain, setShowCurtain] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    const curtainShown = localStorage.getItem("curtain-shown");
+
+    if (!curtainShown) {
+      setShowCurtain(true);
+      localStorage.setItem("curtain-shown", "true");
+    }
+  }, []);
+
   return (
     <>
-      <Curtain />
+      {showCurtain && <Curtain />}
       <Header />
       {children}
       <Footer />
