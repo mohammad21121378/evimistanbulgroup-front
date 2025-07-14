21763cc2955d05087ec8fd7113bece428a00678e
Abolfazl <abofazllramshini@gmail.com>
2025-07-14 16:40:26 +0330

    fixed bugs

--- constants/icons.tsx
+++ constants/icons.tsx
@@ -1041,7 +1041,7 @@ const Location2 = (
 </svg>
 )
 
-const ArrowRight = (<svg width="16" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+const ArrowRight = (<svg width="1rem" viewBox="0 0 16 14" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M1 7H15M15 7L9 1M15 7L9 13" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 )
