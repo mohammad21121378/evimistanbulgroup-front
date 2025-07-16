b5769abb25ddf3367150bcb1cad168eb74f95cde
Abolfazl <abofazllramshini@gmail.com>
2025-07-16 23:29:17 +0330

    map section for listings page implemented

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,7 +4,6 @@ import { NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
 import cn from "classnames";
 import "@/styles/index.css";
-import "tippy.js/dist/tippy.css";
 import { Providers } from "../providers";
 import { routing } from '@/i18n/routing';
 import { ReactNode } from "react";
