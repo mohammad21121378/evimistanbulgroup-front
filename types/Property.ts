94c5711c76b9139948866f7a60f927d36ed9bd1d
Abolfazl <abofazllramshini@gmail.com>
2025-07-21 19:26:36 +0330

    fixed bugs

--- types/Property.ts
+++ types/Property.ts
@@ -9,6 +9,12 @@ export type PropertyRawType = {
   status: string;
   latitude: number;
   longitude: number;
+  url: string;
+  special_features: {title: string}[];
+  locationID: number;
+  min_price: number;
+  max_price: number;
+  created_at: string;
   features: {
     id: number;
     icon: string;
@@ -17,21 +23,6 @@ export type PropertyRawType = {
   }[];
 };
 export type PropertyType = {
-  item: {
-    id: number;
-    images: string[];
-    title: string;
-    price: number;
-    description: string;
-    category: string;
-    location: string;
-    status: string;
-    features: {
-      id: number;
-      icon: string;
-      name: string;
-      value: string | number;
-    }[];
-  };
+  item: PropertyRawType;
   className?: string
 };
