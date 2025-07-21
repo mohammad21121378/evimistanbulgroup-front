f3b00899ac7f8d8f58fae5d5e07e602aca16629d
Abolfazl <abofazllramshini@gmail.com>
2025-07-21 19:41:46 +0330

    fixed bugs for type items

--- app/[locale]/properties-for-sale-in-turkey/page.tsx
+++ app/[locale]/properties-for-sale-in-turkey/page.tsx
@@ -1,9 +1,10 @@
 import ListingsPage from "@/screens/listings";
 import fetchProperties from "@/helpers/api/property/properties";
+import { ListingsType } from "@/types/Property";
 
 export default async function Listings({ searchParams }: { searchParams: { page?: string } }) {
   const page = parseInt(searchParams.page || "1", 10);
-  const listings = await fetchProperties(3, page);
+  const listings = await fetchProperties(3, page) as ListingsType;
 
   return <ListingsPage listings={listings} />;
 }
--- screens/listings/index.tsx
+++ screens/listings/index.tsx
@@ -2,13 +2,13 @@
 
 import Layout from "@/components/Layout";
 import React from "react";
-import Hero from "./hero";
 import Benefits from "./benefits";
 import FindProperty from "./find-property";
 import Content from "./content";
 import { useEffect } from 'react';
+import { ListingsType } from "@/types/Property";
 
-export default function ListingsPage({ listings }: { listings: any[] }) {
+export default function ListingsPage({ listings }: { listings: ListingsType }) {
     useEffect(() => {
         console.log('Listings on client:', listings); // این در مرورگر لاگ می‌شه
     }, [listings]);
