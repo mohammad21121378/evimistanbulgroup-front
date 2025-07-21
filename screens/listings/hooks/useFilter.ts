572a5db452012fbf4b57320e4329c531425a0548
mohammad21121378 <sanatomadan1234@gmail.com>
2025-07-21 18:02:42 +0430

    dynamic properties

--- screens/listings/hooks/useFilter.ts
+++ screens/listings/hooks/useFilter.ts
@@ -77,6 +77,7 @@ export function useFilter({ onFilterByChange = false,listings }: Props) {
     // مهندس اینجا api بزن و properties رو ست کن
 
 
+
     const listings = await fetchProperties(15,currentPage,{
       priceRange,
       locationsSelected,
