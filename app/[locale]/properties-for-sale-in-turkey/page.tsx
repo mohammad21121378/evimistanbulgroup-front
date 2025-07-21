420a7daa2722c8e7b9b6af30a667f5e54a86fb2f
mohammad21121378 <sanatomadan1234@gmail.com>
2025-07-21 18:40:45 +0430

    dynamic properties

--- app/[locale]/properties-for-sale-in-turkey/page.tsx
+++ app/[locale]/properties-for-sale-in-turkey/page.tsx
@@ -1,9 +1,9 @@
 import ListingsPage from "@/screens/listings";
-import fetchProperties from "@/helpers/api/property/properties"
-export default async function Listings() {
+import fetchProperties from "@/helpers/api/property/properties";
 
-  const listings = await fetchProperties(3,1);
-
+export default async function Listings({ searchParams }: { searchParams: { page?: string } }) {
+  const page = parseInt(searchParams.page || "1", 10);
+  const listings = await fetchProperties(3, page);
 
   return <ListingsPage listings={listings} />;
 }
