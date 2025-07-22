2152b7b0eb8b7bb0cdbba21742e83675a909048e
mohammad21121378 <sanatomadan1234@gmail.com>
2025-07-22 23:23:14 +0430

    add locale to listing

--- types/Property.ts
+++ types/Property.ts
@@ -57,11 +57,11 @@ export type PropertyRawType = {
     value: string | number;
   }[];
   agent:{
-    image:string| null,
+    image:string | null,
     name: string | null,
     email: string | null,
     phone: string | null,
-  }[],
+  },
   key_features_and_highlights: string | null;
   investment_and_payment: string | null;
   amenities_and_services: string | null;
