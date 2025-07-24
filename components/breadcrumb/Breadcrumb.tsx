3f6adcac3ce52009176eea75042ad6d9693a27d0
Abolfazl <abofazllramshini@gmail.com>
2025-07-24 23:43:20 +0330

    client-stories implemented

--- components/breadcrumb/Breadcrumb.tsx
+++ components/breadcrumb/Breadcrumb.tsx
@@ -14,19 +14,21 @@ type BreadcrumbProps = {
   lightThem?: boolean
 }
 
-const separator = (
-  <svg
-    height="1.5rem"
-    viewBox="0 0 1 24"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-    className="mx-4"
-  >
-    <line x1="0.5" y1="0" x2="0.5" y2="24" stroke="#64748B" />
-  </svg>
-)
 
 export const Breadcrumb = ({ items, lightThem }: BreadcrumbProps) => {
+  
+  const separator = (
+    <svg
+      height="1.5rem"
+      viewBox="0 0 1 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="mx-4"
+    >
+      <line x1="0.5" y1="0" x2="0.5" y2="24" stroke={lightThem ? "#ffff" : "#64748B"} />
+    </svg>
+  )
+
   const pathname = usePathname()
 
   const autoItems: BreadcrumbItem[] = pathname
