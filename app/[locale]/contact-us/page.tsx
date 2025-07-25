4247c0f17552eb5950f6cccb390588b822c19b22
mohammad21121378 <sanatomadan1234@gmail.com>
2025-07-25 20:59:08 +0430

    add pages metas

--- app/[locale]/contact-us/page.tsx
+++ app/[locale]/contact-us/page.tsx
@@ -1,4 +1,9 @@
 import ContactUs from "@/screens/contact-us";
+import {createGenerateMetadata} from "@/hooks/createGeneratePagesMetadata"
+
+
+export const generateMetadata = createGenerateMetadata("contact-us");
+
 
 export default function Contact() {
   return <ContactUs />;
--- app/[locale]/our-insights/page.tsx
+++ app/[locale]/our-insights/page.tsx
@@ -1,4 +1,9 @@
 import OurInsights from "@/screens/our-insights/pages";
+import {createGenerateMetadata} from "@/hooks/createGeneratePagesMetadata"
+
+
+export const generateMetadata = createGenerateMetadata("our-insights");
+
 
 export default function Home() {
 
