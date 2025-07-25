976ebb23104df1ebe989b4dcfb454a5744a4e3ce
mohammad21121378 <sanatomadan1234@gmail.com>
2025-07-25 20:54:43 +0430

    add pages metas

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import { buildMetadataFromPage } from "@/lib/metadata";
 import HomePage from "@/screens/home";
 import { PageMeta } from "@/types/Seo";
 import {fetchPageContent} from "@/helpers/api/page-detail"
-import {createGenerateMetadata} from "@/hooks/createGenerateMetadata"
+import {createGenerateMetadata} from "@/hooks/createGeneratePagesMetadata"
 
 export const generateMetadata = createGenerateMetadata("home");
 
