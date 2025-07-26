19695df10d742e6eaaa2960d7c226c52c4c4d256
mohammad21121378 <sanatomadan1234@gmail.com>
2025-07-27 00:05:53 +0430

    dynamic articles

--- app/[locale]/our-story/page.tsx
+++ app/[locale]/our-story/page.tsx
@@ -1,4 +1,9 @@
 import AboutPage from "@/screens/about";
+import {createGenerateMetadata} from "@/hooks/createGeneratePagesMetadata"
+
+
+export const generateMetadata = createGenerateMetadata("our-story");
+
 
 export default function About() {
   return <AboutPage />;
