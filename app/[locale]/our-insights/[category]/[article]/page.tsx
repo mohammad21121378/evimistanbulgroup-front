0e90903c9333da4314a797de93796382df39b573
mohammad21121378 <sanatomadan1234@gmail.com>
2025-07-27 00:25:33 +0430

    dynamic articles

--- app/[locale]/our-insights/[category]/[article]/page.tsx
+++ app/[locale]/our-insights/[category]/[article]/page.tsx
@@ -2,6 +2,7 @@ import InsightDetails from "@/screens/our-insights/insight-details";
 import {createGenerateArticleMetadata} from "@/hooks/createGenerateArticleMetadata"
 import {fetchArticle} from "@/helpers/api/article"
 import {Article} from "../../../../../types/Article";
+import {notFound} from "next/navigation";
 
 interface Props {
   params: {
@@ -19,6 +20,7 @@ interface ArticleResponse {
 export default async function Page({ params }: Props) {
   const { locale,article } = params;
   const articleData = await fetchArticle(article, locale) as ArticleResponse;
+  if(articleData.article)  return notFound()
 
   return <InsightDetails articleData={articleData} />;
 }
--- hooks/createGenerateArticleMetadata.ts
+++ hooks/createGenerateArticleMetadata.ts
@@ -11,21 +11,23 @@ export function createGenerateArticleMetadata() {
     return async function generateMetadata({ params }: GenerateMetadataProps) {
         const { locale,article } = params;
         const {article:articleData} = await fetchArticle(article, locale);
+        if(articleData){
+            const meta: PageMeta = {
+                meta_title: articleData?.meta_title || "",
+                meta_description: articleData?.meta_description || "",
+                meta_follow: articleData?.meta_follow || "follow",
+                meta_index: articleData?.meta_index || "index",
+                url: articleData?.url ,
+                updated_at: articleData?.updated_at || new Date().toISOString(),
+                image:
+                    articleData?.media?.url ||
+                    "https://evimistanbulgroup.com/_next/image?url=%2Fimages%2FEvimIstanbul%20Group%20Official%20LOGO.png&w=2048&q=75",
+                language: articleData?.language,
+                type: "website",
+            };
 
-        const meta: PageMeta = {
-            meta_title: articleData?.meta_title || "",
-            meta_description: articleData?.meta_description || "",
-            meta_follow: articleData?.meta_follow || "follow",
-            meta_index: articleData?.meta_index || "index",
-            url: articleData?.url ,
-            updated_at: articleData?.updated_at || new Date().toISOString(),
-            image:
-                articleData?.media?.url ||
-                "https://evimistanbulgroup.com/_next/image?url=%2Fimages%2FEvimIstanbul%20Group%20Official%20LOGO.png&w=2048&q=75",
-            language: articleData?.language,
-            type: "website",
-        };
+            return buildMetadataFromPage(meta);
+        }
 
-        return buildMetadataFromPage(meta);
     };
 }
