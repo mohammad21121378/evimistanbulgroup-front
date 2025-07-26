bdf3e76598c8453bbc45fba53a1f8738d7e9c187
mohammad21121378 <sanatomadan1234@gmail.com>
2025-07-26 23:44:49 +0430

    dynamic articles

--- screens/our-services-detail/insights/insights.tsx
+++ screens/our-services-detail/insights/insights.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styles from "./insights.module.css";
 import cn from "classnames";
 import { Heading } from "@/components/typography";
@@ -6,9 +6,31 @@ import { ArrowRight } from "@/constants/icons";
 import Link from "next/link";
 import { useInsights } from "@/constants/guides-and-Insights.constants";
 import GuidesAndInsightsIisting from "@/components/guides-and-Insights-listing";
+import {Article} from "../../../types/Article";
+import {useLocale} from "next-intl";
+import { fetchRecentArticles } from "@/helpers/api/recent-articles";
 
+
+type ArticleResponse = {
+  articles: Article[];
+};
 export default function Insights() {
+  const [articles, setArticles] = useState<Article[]>([]);
+  const locale = useLocale();
 
+
+  useEffect(() => {
+    const getArticles = async () => {
+      try {
+        const result = await fetchRecentArticles(3, 1, locale) as ArticleResponse;
+        setArticles(result.articles);
+      } catch (error) {
+        console.error("", error);
+      }
+    };
+
+    getArticles();
+  }, [locale]);
   return (
     <section className={cn("section")}>
       <div className={cn("container")}>
@@ -17,14 +39,14 @@ export default function Insights() {
             <Heading type="heading-3">Our Guides & Insights</Heading>
           </h2>
 
-          <Link href="/properties-for-sale-in-turkey" className={cn("button button-primary")}>
+          <Link href="/our-insights" className={cn("button button-primary")}>
             View All Insights {ArrowRight}
           </Link>
           
         </div>
 
         <div className="grid sm:grid-cols-2 grid-cols-1 md:grid-cols-3 gap-4 mt-14">
-          {useInsights()?.map((item, index) => (
+          {articles?.map((item, index) => (
             <GuidesAndInsightsIisting key={index} {...item} />
           ))}
         </div>
