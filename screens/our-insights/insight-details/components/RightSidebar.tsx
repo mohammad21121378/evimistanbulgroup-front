493571c0e9b3c146fc56502df8853456f1a0a0b8
mohammad21121378 <sanatomadan1234@gmail.com>
2025-07-26 23:38:05 +0430

    dynamic articles

--- screens/our-insights/insight-details/components/RightSidebar.tsx
+++ screens/our-insights/insight-details/components/RightSidebar.tsx
@@ -1,7 +1,11 @@
 import UsefulLinks from "@/components/useful-links";
 import {Article} from "../../../../types/Article";
 
-export default function RightSidebar({article}: Article) {
+type Props = {
+    article: Article;
+};
+
+export default function RightSidebar({article}: Props) {
     return (
         <div className="md:col-span-3 md:pt-16 pt-2">
             <div className="bg-slate-100 outline w-full text-center outline-1 outline-orange-500 p-3.5 rounded-lg grid justify-center text-base font-medium">
