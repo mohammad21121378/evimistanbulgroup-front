1f0d4d13f525ef76eb6ba6a10b2abe5139c0891e
mohammad21121378 <sanatomadan1234@gmail.com>
2025-07-27 00:48:58 +0430

    dynamic articles

--- app/[locale]/our-services/[slug]/page.tsx
+++ app/[locale]/our-services/[slug]/page.tsx
@@ -2,11 +2,15 @@ import OurServicesDetails from "@/screens/our-services-detail";
 import { servicesData } from "@/screens/our-services-detail/constants";
 
 import { notFound } from "next/navigation";
+import {createGenerateMetadata} from "@/hooks/createGeneratePagesMetadata"
 
 type Props = {
   params: { slug: string };
 };
 
+
+
+export const generateMetadata = createGenerateMetadata("our-services");
 
 
 export function generateStaticParams() {
