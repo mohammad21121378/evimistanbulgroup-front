6b4e65bff02da01bbcdb8c8c83f5291a354f5fe3
mohammad21121378 <sanatomadan1234@gmail.com>
2025-07-27 21:33:49 +0430

    add related insight to propery

--- screens/property-detail/related-insights/related-insights.tsx
+++ screens/property-detail/related-insights/related-insights.tsx
@@ -7,28 +7,33 @@ import Link from "next/link";
 import { useInsights } from "@/constants/guides-and-Insights.constants";
 import GuidesAndInsightsIisting from "@/components/guides-and-Insights-listing";
 import Button from "@/components/ui/Button";
+import {Article} from "../../../types/Article";
 
-export default function RelatedInsights() {
+
+interface Props{
+  articles:Article[]
+}
+export default function RelatedInsights({articles}:Props) {
 
   return (
     <section className={cn("section")}>
-      <div className={cn("container")}>
+      {articles.length > 0 && <div className={cn("container")}>
         <div className={styles.content}>
           <h2>
             <Heading type="heading-3">Related Guides and Insights</Heading>
           </h2>
 
           <Button flex size="auto" href="/our-insights" className={cn("button button-primary")}>
-            View All  Similar Guides and Insights {ArrowRight}
+            View All Guides and Insights {ArrowRight}
           </Button>
         </div>
 
-        {/*<div className="grid sm:grid-cols-2 grid-cols-1 md:grid-cols-3 gap-4 mt-14">
-          {useInsights()?.map((item, index) => (
-            <GuidesAndInsightsIisting key={index} {...item} />
+        <div className="grid sm:grid-cols-2 grid-cols-1 md:grid-cols-3 gap-4 mt-14">
+          {articles?.map((item, index) => (
+              <GuidesAndInsightsIisting key={index} {...item} />
           ))}
-        </div>*/}
-      </div>
+        </div>
+      </div>}
     </section>
   );
 }
