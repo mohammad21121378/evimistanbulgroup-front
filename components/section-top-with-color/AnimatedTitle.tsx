b8a0fbc6b537e8f68f0e06dea7b3aa601602d0d6
Abolfazl <abofazllramshini@gmail.com>
2025-07-27 14:04:27 +0330

    fixed bugs for title

--- components/section-top-with-color/AnimatedTitle.tsx
+++ components/section-top-with-color/AnimatedTitle.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useMediaQuery } from 'react-responsive';
 import classNames from 'classnames'
 import { motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
@@ -12,6 +13,9 @@ interface AnimatedTitleProps {
 }
 
 const AnimatedTitle = ({ title, noneAnimate, className, titleSm }: AnimatedTitleProps) => {
+
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+
   const [start, setStart] = useState(false)
 
   useEffect(() => {
@@ -38,87 +42,50 @@ const AnimatedTitle = ({ title, noneAnimate, className, titleSm }: AnimatedTitle
 
   const lines = title.split('\n')
   const words = title.split(' ')
-  let charIndex = 0
+  let charIndex = 0;
+
+
 
   return (
     <div className={classNames("title-wrapper relative inline-block overflow-hidden mt-auto xl:pr-28 text-white", className)}>
 
       <motion.h1
-        className={classNames("font-bold  leading-[105%] mt-auto  relative z-10 md:hidden",
-        {
-          "xl:text-[7rem] md:text-[6.6rem] sm:text-[5rem] text-[4rem]": !titleSm,
-          "xl:text-[6rem] md:text-[5.7rem] sm:text-[5rem] text-[4rem]": titleSm,
-        }
+        className={classNames("font-bold  leading-[105%] mt-auto relative z-10 ",
+          {
+            "xl:text-[7rem] md:text-[6.6rem] sm:text-[5rem] text-[4rem]": !titleSm,
+            "xl:text-[6rem] md:text-[5.7rem] sm:text-[5rem] text-[4rem]": titleSm,
+          }
         )}
         variants={container}
         initial="hidden"
         animate={start ? 'show' : 'hidden'}
       >
-        {words.map((word, wi) => (
-          <span
-            key={wi}
-            className="inline-block whitespace-nowrap"
-          >
-            
-            {word.split('').map((char, ci) => {
-              const delay = 0.1 * charIndex++
-              return (
-                <motion.span
-                key={ci}
-                variants={letter} 
-                className={classNames("inline-block relative ", { "fade-loop":  !noneAnimate})}
-                style={{ animationDelay: `${4 + delay}s` }}
-              >
-                {char}
-              </motion.span>
-              )
-            })}
 
-            
-
-            {wi < words.length - 1 && (
-              <motion.span
-                variants={letter}
-                className="inline-block"
-                style={{ width: '0.25em' }}
-              >
-                &nbsp;
-              </motion.span>
-            )}
-          </span>
-        ))}
-      </motion.h1>
-
-      <motion.h1
-        className={classNames("font-bold  leading-[105%] mt-auto  relative z-10 hidden md:block",
         {
-          "xl:text-[7rem] md:text-[6.6rem] sm:text-[5rem] text-[4rem]": !titleSm,
-          "xl:text-[6rem] md:text-[5.7rem] sm:text-[5rem] text-[4rem]": titleSm,
-        }
-        )}
-        variants={container}
-        initial="hidden"
-        animate={start ? 'show' : 'hidden'}
-      >        
-        {lines.map((line, li) => {
-          const words = line.split(' ')
-          return (
-            <div key={li} className="block">
+          isMobile ?
+            <>
               {words.map((word, wi) => (
-                <span key={wi} className="inline-block whitespace-nowrap md:pb-2.5">
+                <span
+                  key={wi}
+                  className="inline-block whitespace-nowrap"
+                >
+
                   {word.split('').map((char, ci) => {
                     const delay = 0.1 * charIndex++
                     return (
                       <motion.span
                         key={ci}
                         variants={letter}
-                        className={classNames("inline-block relative ", { "fade-loop":  !noneAnimate})}
+                        className={classNames("inline-block relative ", { "fade-loop": !noneAnimate })}
                         style={{ animationDelay: `${4 + delay}s` }}
                       >
                         {char}
                       </motion.span>
                     )
                   })}
+
+
+
                   {wi < words.length - 1 && (
                     <motion.span
                       variants={letter}
@@ -130,10 +97,47 @@ const AnimatedTitle = ({ title, noneAnimate, className, titleSm }: AnimatedTitle
                   )}
                 </span>
               ))}
-            </div>
-          )
-        })}
+            </>
+            :
+            <>
+              {lines.map((line, li) => {
+                const words = line.split(' ')
+                return (
+                  <div key={li} className="block">
+                    {words.map((word, wi) => (
+                      <span key={wi} className="inline-block whitespace-nowrap md:pb-2.5">
+                        {word.split('').map((char, ci) => {
+                          const delay = 0.1 * charIndex++
+                          return (
+                            <motion.span
+                              key={ci}
+                              variants={letter}
+                              className={classNames("inline-block relative ", { "fade-loop": !noneAnimate })}
+                              style={{ animationDelay: `${4 + delay}s` }}
+                            >
+                              {char}
+                            </motion.span>
+                          )
+                        })}
+                        {wi < words.length - 1 && (
+                          <motion.span
+                            variants={letter}
+                            className="inline-block"
+                            style={{ width: '0.25em' }}
+                          >
+                            &nbsp;
+                          </motion.span>
+                        )}
+                      </span>
+                    ))}
+                  </div>
+                )
+              })}
+            </>
+        }
+
       </motion.h1>
+
     </div>
   )
 }
