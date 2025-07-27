dd47f056edb1a9e9a37de90dcbcff30e725bda1c
Abolfazl <abofazllramshini@gmail.com>
2025-07-27 13:54:43 +0330

    terms-conditions and privacy-policy implemented

--- screens/privacy-policy/components/Hero.tsx
+++ screens/privacy-policy/components/Hero.tsx
@@ -1,11 +1,13 @@
 
+import Breadcrumb from "@/components/breadcrumb";
 import AnimatedTitle from "@/components/section-top-with-color/AnimatedTitle";
 
 export default function Hero() {
     return (
         <section className="section !pt-36 !pb-6">
             <div className="container">
-                <AnimatedTitle className="!text-black" title={`Privacy \nPolicy`} />
+            <Breadcrumb items={[{label: 'Privacy Policy'}]} />
+                <AnimatedTitle className="!text-black !mt-6" title={`Privacy \nPolicy`} />
             </div>
         </section>
     );
--- screens/terms-conditions/components/Hero.tsx
+++ screens/terms-conditions/components/Hero.tsx
@@ -1,11 +1,13 @@
 
+import Breadcrumb from "@/components/breadcrumb";
 import AnimatedTitle from "@/components/section-top-with-color/AnimatedTitle";
 
 export default function Hero() {
     return (
         <section className="section !pt-36 !pb-6">
             <div className="container">
-                <AnimatedTitle className="!text-black" title={`Terms & \nConditions`} />
+                <Breadcrumb items={[{label: 'Terms & Conditions'}]} />
+                <AnimatedTitle className="!text-black !mt-6" title={`Terms & \nConditions`} />
             </div>
         </section>
     );
