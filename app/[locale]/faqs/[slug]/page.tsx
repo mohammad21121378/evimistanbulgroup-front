7621d12b8ca6c2c92e855646c2bcadcef6dcc209
Abolfazl <abofazllramshini@gmail.com>
2025-07-29 13:13:09 +0330

    schama faqs implemented

--- app/[locale]/faqs/[slug]/page.tsx
+++ app/[locale]/faqs/[slug]/page.tsx
@@ -2,12 +2,11 @@ import FaqDetails from "@/screens/faq-details";
 import { faqsData } from "@/screens/faq-details/constants";
 import { createStaticMetadataGenerator } from "@/hooks/createStaticMetadataGenerator"
 import { notFound } from "next/navigation";
+import SchemaJsonLd from "@/components/schema-json-ld";
 
 type Props = {
   params: { slug: string };
 };
-
-// import {createGenerateMetadata} from "@/hooks/createGeneratePagesMetadata"
 
 export const generateMetadata = createStaticMetadataGenerator("faqs");
 
@@ -21,5 +20,28 @@ export default function ServicePage({ params }: Props) {
   const faq = faqsData.find(s => s.slug === params.slug);
   if (!faq) return notFound();
 
-  return <FaqDetails data={faq} />;
+  const faqSchema = {
+    "@context": "https://schema.org",
+    "@type": "FAQPage",
+    "mainEntity": faq.list.map((item) => {
+      const match = item.match(/\*\*(.+?)\*\*\s*[-–—]\s*(.+)/s);
+      if (!match) return null;
+  
+      const [, question, answer] = match;
+      return {
+        "@type": "Question",
+        "name": question.trim(),
+        "acceptedAnswer": {
+          "@type": "Answer",
+          "text": answer.trim(),
+        },
+      };
+    }).filter(Boolean)
+  };
+
+  return (<>
+  <SchemaJsonLd data={faqSchema} />
+  <FaqDetails data={faq} />
+  </>
+  );
 }
