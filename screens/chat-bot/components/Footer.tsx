95945dd8cfd063ed12d5474fd7389001ae91fb60
Abolfazl <abofazllramshini@gmail.com>
2025-07-30 19:37:00 +0330

    chat bot completed

--- screens/chat-bot/components/Footer.tsx
+++ screens/chat-bot/components/Footer.tsx
@@ -39,7 +39,7 @@ export default function Footer({ sendMessage, input, setInput, isTyping, activel
                         interactive={true}
                     >
                         <motion.button
-                        key={activeCleanButton}
+                        
                             initial={{ opacity: 0, x: -10 }}
                             animate={{ opacity: 1, x: 0 }}
                             exit={{ opacity: 0, x: -10 }}
