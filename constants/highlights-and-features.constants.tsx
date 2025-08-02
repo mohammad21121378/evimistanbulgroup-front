e8e23f41b7344b31a85387fba571b412cb4457d7
Abolfazl <abofazllramshini@gmail.com>
2025-08-02 22:18:35 +0330

    fixed bugs for  linked to properties-for-sale-in-turkey

--- constants/highlights-and-features.constants.tsx
+++ constants/highlights-and-features.constants.tsx
@@ -5,28 +5,29 @@ export function useHighlights() {
 
   return [
     {
-      id: "183.188",
+      id: "feat-value-investment-tags[best-for-investment]",
       title: t("investment"),
       image: "3.jpg",
       bgColor: "bg-[#CC2D4A]",
     },
     {
-      id: "183.190",
+      id: "feat-value-investment-tags[installment-available]",
       title: t("installment"),
       image: "2.jpg",
       bgColor: "bg-[#BC1A6E]",
     },
     {
-      id: "184.193",
+      id: "feat-citizenship-tags[approved-for-citizenship]",
       title: t("citizenship"),
       image: "1.jpg",
       bgColor: "bg-[#DE3151]",
     },
     {
-      id: "187.207",
+      id: "feat-urgency-tags[early-stage-project]",
       title: t("earlyStage"),
       image: "4.jpg",
       bgColor: "bg-[#D93B30]",
     },
   ];
 }
+
